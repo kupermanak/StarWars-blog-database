7d9d48a049cb5792a0267be00cdab9ecbe2b10a9
kupermanak <kupermanak@gmail.com>
2022-03-30 11:40:25 +0000

    1st update

--- src/models.py
+++ src/models.py
@@ -8,23 +8,78 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
+class Characters(Base):
+    __tablename__ = 'characters'
+    uid = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    height = Column(String(50)) 
+    mass = Column(String(50))
+    hair_color = Column(String(50))
+    eye_color = Column(String(50))
+    birth_year = Column(String(50))
+    gender = Column(String(50))
+    homeworld = Column(String(50))
+    url = Column(String(50))
+    description = Column(String(50))
+    _id = Column(String(50))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Planets(Base):
+    __tablename__ = 'planets'
+    uid = Column(Integer, primary_key=True)
+    diameter = Column(String(50))
+    rotation_period = Column(String(50))
+    orbital_period = Column(String(50))
+    gravity = Column(String(50))
+    population = Column(String(50))
+    climate = Column(String(50))
+    terrain = Column(String(50))
+    surface_water = Column(String(50))
+    name = Column(String(50))
+    url = Column(String(50))
+    description = Column(String(50))
+    _id = Column(String(50))
+    characters_uid = Column(Integer, ForeignKey('characters.id'))
+    characters = relationship(Characters)
+
+class Starships(Base):
+    __tablename__ = 'starships'
+    uid = Column(Integer, primary_key=True)
+    model = Column(String(50))
+    starship_class = Column(String(50))
+    manufacturer = Column(String(50))
+    cost_in_credits = Column(String(50))
+    length = Column(String(50))
+    crew = Column(String(50))
+    passengers = Column(String(50))
+    max_atmosphering_speed = Column(String(50))
+    hyperdrive_rating = Column(String(50))
+    MGLT = Column(String(50))
+    cargo_capacity = Column(String(50))
+    consumables = Column(String(50))
+    name = Column(String(50))
+    url = Column(String(50))
+    description = Column(String(50))
+    _id = Column(String(50))
+
+class Pilots(Base):
+    __tablename__ = "pilots"
+    pilot_uid = Column(Integer, primary_key=True)
+    pilot_name = Column(String(50), ForeignKey('characters.id'))
+    characters = relationship(Characters)
+    starship_name = Column(String(50), ForeignKey('starships.id'))
+    starships = relationship(Starships)
+
+class Favorites(Base):
+    __tablename__ = "favorites"
+    Fav_Charact = Column(String(50), ForeignKey('characters.id'), primary_key=True)
+    Name_charact = Column(String(50), ForeignKey('characters.name'))
+    characters = relationship(Characters)
+    Fav_Planets = Column(String(50), ForeignKey('planets.id'), primary_key=True)
+    Name_planet = Column(String(50), ForeignKey('planets.name'))
+    planets = relationship(Planets)
+    Fav_Starships = Column(String(50), ForeignKey('starships.id'), primary_key=True)
+    Name_Starship = Column(String(50), ForeignKey('starships.name'))
+    starships = relationship(Starships)
 
     def to_dict(self):
         return {}
